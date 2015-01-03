98b7fedcba557279b494883dcaaff37d298c614f
Beau Beaudry <beau.beaudry@gmail.com>
2015-01-02 18:40:21 -0800

    Add unit test for Set1/Problem1

--- CryptoChallenges/Set1/Problem1.cs
+++ CryptoChallenges/Set1/Problem1.cs
@@ -5,17 +5,9 @@ namespace CryptoChallenges.Set1
 {
     public static class Problem1
     {
-        public static void Solve()
+        public static string Solve(string input)
         {
-            const string input =
-               "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-
-            const string expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-
-            var encoded = HexString.Base64Encode(input);
-
-            Console.WriteLine("Expected: {0}", expected);
-            Console.WriteLine("Result: {0}", encoded);
+            return HexString.Base64Encode(input);
         }
     }
 }
---
+++ CryptoChallengesTests/Set1Tests.cs
@@ -0,0 +1,20 @@
+﻿using CryptoChallenges.Set1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CryptoChallengesTests
+{
+    [TestClass]
+    public class Set1Tests
+    {
+        [TestMethod]
+        public void Problem1_Solve_ResultMatchesExpected()
+        {
+            const string input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+            const string expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+            var result = CryptoChallenges.Set1.Problem1.Solve(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
