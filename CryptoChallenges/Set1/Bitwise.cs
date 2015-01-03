e32abfbe731f2f243b9eab48c54680c8fdab3ccd
Beau Beaudry <beau.beaudry@gmail.com>
2015-01-02 18:00:46 -0800

    Add Bitwise class and XORArray function

---
+++ CryptoChallenges/Set1/Bitwise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CryptoChallenges.Set1
+{
+    internal class Bitwise
+    {
+
+        /// <summary>
+        /// Returns the XOR combination of two buffers.
+        /// </summary>
+        internal static byte[] XORArray(byte[] a, byte[] b)
+        {
+            if (a.Length != b.Length)
+                throw new ArgumentException("Both arrays must be the same length");
+
+            var result = new byte[a.Length];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                result[i] = (byte)(a[i] ^ b[i]);
+            }
+
+            return result;
+        }
+    }
+}
