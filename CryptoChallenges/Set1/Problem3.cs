5f7f9b80505e5a79318524fbc8c214b94a249f79
Beau Beaudry <beau.beaudry@gmail.com>
2015-01-04 15:07:52 -0800

    Setup Set1/Problem3

---
+++ CryptoChallenges/Set1/Problem3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoChallenges.Set1
+{
+    public static class Problem3
+    {
+        public static string Solve(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
