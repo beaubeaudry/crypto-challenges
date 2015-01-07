eac9672d5083e043b3819d086bbca16de5b00230
Beau Beaudry <beau.beaudry@gmail.com>
2015-01-06 19:29:14 -0800

    Fix whitespace in Program.cs

--- CryptoChallenges/Program.cs
+++ CryptoChallenges/Program.cs
@@ -11,7 +11,7 @@ namespace CryptoChallenges
     {
         static void Main(string[] args)
         {
-           
+            Problem3.Solve("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
         }
     }
 }
