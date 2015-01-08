47e5ad90dca861c89f714bf3e020c008cb2c6139
Beau Beaudry <beau.beaudry@gmail.com>
2015-01-07 20:40:36 -0800

    Add StringScorer class and refactor Set1/Problem3

--- CryptoChallenges/Set1/Problem3.cs
+++ CryptoChallenges/Set1/Problem3.cs
@@ -20,7 +20,7 @@ namespace CryptoChallenges.Set1
             {
                 byte[] xoredBytes = Bitwise.XORArray(bytes, key);
                 string xoredString = Encoding.ASCII.GetString(xoredBytes);
-                double score = ScoreString(xoredString);
+                double score = StringScorer.ScoreByLetterFrequency(xoredString);
 
                 if (score > topScore)
                 {
@@ -31,43 +31,5 @@ namespace CryptoChallenges.Set1
 
             return topString;
         }
-        
-        // Case-insensitive. 
-        public static double ScoreString(string input)
-        {
-            // This produces the sum of the scores for each character. 
-            return input.ToLower().Where(CharacterScores.ContainsKey).Sum(c => CharacterScores[c]);
-        }
-
-        static readonly Dictionary<char, float> CharacterScores = new Dictionary<char, float>()
-        {
-            {' ', 5.0f},
-            {'a', 8.167f},
-            {'b', 1.492f},
-            {'c', 2.782f},
-            {'d', 4.253f},
-            {'e', 12.702f},
-            {'f', 2.228f},
-            {'g', 2.015f},
-            {'h', 6.094f},
-            {'i', 6.966f},
-            {'j', 0.153f},
-            {'k', 0.772f},
-            {'l', 4.025f},
-            {'m', 2.406f},
-            {'n', 6.749f},
-            {'o', 7.507f},
-            {'p', 1.929f},
-            {'q', 0.095f},
-            {'r', 5.987f},
-            {'s', 6.326f},
-            {'t', 9.056f},
-            {'u', 2.758f},
-            {'v', 0.978f},
-            {'w', 2.360f},
-            {'x', 0.150f},
-            {'y', 1.974f},
-            {'z', 0.074f}
-        };
     }
 }
