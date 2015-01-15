6cc0fc93b4db6723a3a152b2d064f9c7214bb92b
Beau Beaudry <beau.beaudry@gmail.com>
2015-01-14 18:49:51 -0800

    Add Permutations class and SingleByteXOR method

---
+++ CryptoChallenges/Set1/Permutations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CryptoChallenges.Set1
+{
+        /// <summary>
+        /// Utility class to generate permutations.
+        /// </summary>
+        public static class Permutations
+        {
+            /// <summary>
+            /// XOR a string by each byte from 0 to 255 and return each result.
+            /// </summary>
+            public static IEnumerable<string> SingleByteXOR(string value)
+            {
+                for (byte key = 0; key < 255; key++)
+                {
+                    yield return Bitwise.SingleByteXOR(value, key);
+                }
+            }
+        }
+}
