6cbbadc413048925f5fbf8a93ef85c1ae8c526e2
Beau Beaudry <beau.beaudry@gmail.com>
2015-01-15 14:23:02 -0800

    Implement Set1/Problem5

--- CryptoChallenges/Set1/Problem5.cs
+++ CryptoChallenges/Set1/Problem5.cs
@@ -4,9 +4,17 @@ namespace CryptoChallenges.Set1
 {
     public static class Problem5
     {
-        public static string Solve()
+        public static string Solve(string input, string key)
         {
-            throw new NotImplementedException();
+            var encoded = Bitwise.RepeatingByteXOR(input, key);
+
+            return ByteArrayToString(encoded);
+        }
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            string hex = BitConverter.ToString(ba);
+            return hex.Replace("-", "");
         }
     }
 }
