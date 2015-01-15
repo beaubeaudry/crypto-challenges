3b3b40dc69cdeb1c37a2bfba4b648652290dfe33
Beau Beaudry <beau.beaudry@gmail.com>
2015-01-15 15:05:46 -0800

    Add CountSetBits function

--- CryptoChallenges/Set1/Bitwise.cs
+++ CryptoChallenges/Set1/Bitwise.cs
@@ -65,5 +65,16 @@ namespace CryptoChallenges.Set1
 
             return array;
         }
+
+        /// <summary>
+        /// Counts the number of set bits in an integer.
+        /// </summary>
+        public static int CountSetBits(int i)
+        {
+            // Source: http://stackoverflow.com/questions/109023/how-to-count-the-number-of-set-bits-in-a-32-bit-integer
+            i = i - ((i >> 1) & 0x55555555);
+            i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
+            return (((i + (i >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+        }
     }
 }
