bb9068b5e0fbc0caddc50ed299dd804d68ca5ff8
Beau Beaudry <beau.beaudry@gmail.com>
2015-01-14 18:50:55 -0800

    Refactor Set1.Problem3 solution

--- CryptoChallenges/Set1/Problem3.cs
+++ CryptoChallenges/Set1/Problem3.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace CryptoChallenges.Set1
 {
     public static class Problem3
@@ -9,27 +6,19 @@ namespace CryptoChallenges.Set1
         public static string Solve(string input)
         {
             double topScore = 0.0;
-            string topString = "";
+            string bestResult = "";
 
-            // 
-            var bytes = HexString.ToByteArray(input);
-            
-            // For each potential key value we XOR the input string by the
-            // key, convert back to a string, and return the scored result.
-            for (byte key = 0; key < 255; key++)
+            foreach (string permutation in Permutations.SingleByteXOR(input))
             {
-                byte[] xoredBytes = Bitwise.XORArray(bytes, key);
-                string xoredString = Encoding.ASCII.GetString(xoredBytes);
-                double score = StringScorer.ScoreByLetterFrequency(xoredString);
-
+                double score = StringScorer.ScoreByLetterFrequency(permutation);
                 if (score > topScore)
                 {
                     topScore = score;
-                    topString = xoredString;
+                    bestResult = permutation;
                 }
             }
 
-            return topString;
+            return bestResult;
         }
     }
 }
